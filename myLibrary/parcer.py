6b434505453ea20e8608ee4cd5c2367ce6ad46fb
0neF0rA11 <sanya.oleinickov22@yandex.ru>
2024-02-13 12:20:32 +0300

    Step1: data collection and processing

---
+++ myLibrary/parcer.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+df=pd.read_json('quotes/quotes.json')
+unique_quotes = df['Quote'].unique()
+
+result_df = pd.DataFrame(unique_quotes, columns=['Quote'])
+result_df = result_df.sample(n=2500)
+
+result_df.Quote.to_csv('file.txt', index=False, header=False)
